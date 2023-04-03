7543462aa1e0ace19d322d6bd43b41c741a9ea78
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-04-03 15:35:03 +0700

    1234

--- workshops/tokioPOS/src/app/components/register/register.component.ts
+++ workshops/tokioPOS/src/app/components/register/register.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  username = '';
 
+  handleUsernameChanged(value: string) {
+    debugger;
+    console.log(value);
+  }
 }

4e5e282d3ecd9cd58edfd9a223ff9abf54e38728
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-04-03 11:36:41 +0700

    1234

--- workshops/demo0/src/app/components/c1/c1.component.ts
+++ workshops/demo0/src/app/components/c1/c1.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-c1',
@@ -7,4 +7,5 @@ import { Component, Input } from '@angular/core';
 })
 export class C1Component {
   @Input() subtitle: string = '';
+  @Output() reset = new EventEmitter();
 }

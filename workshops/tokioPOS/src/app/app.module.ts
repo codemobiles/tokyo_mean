331d836c822a6eb18fa00cc3d88907d832cf4e29
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-04-03 13:38:02 +0700

    1234

--- workshops/tokioPOS/src/app/app.module.ts
+++ workshops/tokioPOS/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,6 +15,7 @@ import { MatButtonModule } from '@angular/material/button';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatBadgeModule,
   ],
   providers: [],
   bootstrap: [AppComponent],

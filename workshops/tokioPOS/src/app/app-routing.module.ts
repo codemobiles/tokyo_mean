0c39e618471b84c296a4ef3f82fb14c7eaa16738
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-04-03 15:13:34 +0700

    1234

--- workshops/tokioPOS/src/app/app-routing.module.ts
+++ workshops/tokioPOS/src/app/app-routing.module.ts
@@ -2,10 +2,31 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { ReportComponent } from './components/report/report.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { StockCreateComponent } from './components/stock-create/stock-create.component';
+import { StockEditComponent } from './components/stock-edit/stock-edit.component';
+import { StockComponent } from './components/stock/stock.component';
+import { TransactionComponent } from './components/transaction/transaction.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  // { path: 'extra', component: ExtraComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'stock', component: StockComponent },
+  {
+    path: 'stock/create',
+    component: StockCreateComponent,
+  },
+  {
+    path: 'stock/edit/:id',
+    component: StockEditComponent,
+  },
+  { path: 'shop', component: ShopComponent },
+  { path: 'report', component: ReportComponent },
+  { path: 'transaction', component: TransactionComponent },
+  { path: '**', redirectTo: 'login' }, // undefined pages
 ];
 
 @NgModule({

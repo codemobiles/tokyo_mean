a53fb672156aedd1e5e92b12a00ee040c10cebea
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-04-03 11:11:23 +0700

    1234

--- workshops/demo0/src/app/app.module.ts
+++ workshops/demo0/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { C1Component } from './components/c1/c1.component';
+import { C2Component } from './components/c2/c2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    C1Component
+    C1Component,
+    C2Component
   ],
   imports: [
     BrowserModule,

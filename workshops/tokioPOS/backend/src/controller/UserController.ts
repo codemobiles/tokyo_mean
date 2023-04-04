8e2ba43f4ca0112ca67a81267eb1dee1cc63ae67
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-04-04 14:28:23 +0700

    1234

--- workshops/tokioPOS/backend/src/controller/UserController.ts
+++ workshops/tokioPOS/backend/src/controller/UserController.ts
@@ -10,7 +10,16 @@ export class UserController {
   }
 
   register(request, response, next) {
-    console.log(JSON.stringify(request.body));
-    return this.userRepository.create(request.body);
+    try {
+      req.body.created = savedValue(req.body.created, new Date());
+      req.body.level = savedValue(req.body.level, "normal");
+      req.body.__v = savedValue(req.body.__v, 0);
+
+      req.body.password = await bcrypt.hash(req.body.password, 8);
+      const doc = await this.userRepository.save(req.body);
+      return { result: "ok", message: doc };
+    } catch (e) {
+      return { result: "nok", message: "invalid data" };
+    }
   }
 }

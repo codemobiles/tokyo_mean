58b75ddb53dd8eedae83bad6a7c5d490073b85f8
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-04-04 15:39:40 +0700

    1234

--- workshops/tokioPOS/src/app/services/rest.service.ts
+++ workshops/tokioPOS/src/app/services/rest.service.ts
@@ -1,15 +1,20 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RestService {
+  private headers = new HttpHeaders({ 'Content-Type': 'application/json' });
   constructor(private http: HttpClient) {}
 
   public assets(image: string): string {
     return `/assets/${image}`;
   }
 
-  
+  register(value: any) {
+    return this.http.post<any>('', value, {
+      headers: this.headers,
+    });
+  }
 }

a389721bfde69d9c74c35eca5b82d84de4664ab0
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-04-04 09:41:08 +0700

    1234

--- workshops/tokioPOS/src/app/components/register/register.component.ts
+++ workshops/tokioPOS/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RestService } from 'src/app/services/rest.service';
 
 @Component({
   selector: 'app-register',
@@ -6,6 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  constructor(public rest: RestService) {}
+
   hide = true;
   onSubmit(value: any) {
     alert(JSON.stringify(value));

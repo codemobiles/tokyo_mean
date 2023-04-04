fc26f609cf76e77a42b14f795bf23a5e86076444
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-04-04 09:58:58 +0700

    1234

---
+++ workshops/tokioPOS/backend/index.ts
@@ -0,0 +1,5 @@
+let tmp1: string = 'lek';
+let tmp2: number = 1;
+let tmp3: boolean = true;
+
+console.log(`tmp1: ${tmp1}, tmp2: ${tmp2}, tmp3: ${tmp3}`);
--- workshops/tokioPOS/backend/server.js
+++ workshops/tokioPOS/backend/server.js
@@ -1,1 +1,6 @@
 console.log("Lek");
+tmp1 = 0;
+tmp1 = true;
+tmp1 = "Lek";
+let tmp2: string = "1234";
+// console.log(tmp2);

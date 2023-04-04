58bd940fd513745a14552a754f6f10808239d0c1
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-04-04 13:59:50 +0700

    1234

--- workshops/tokioPOS/backend/src/index.ts
+++ workshops/tokioPOS/backend/src/index.ts
@@ -3,7 +3,6 @@ import * as bodyParser from 'body-parser';
 import { Request, Response } from 'express';
 import { AppDataSource } from './data-source';
 import { Routes } from './routes';
-import { User } from './entity/Users';
 
 AppDataSource.initialize()
   .then(async () => {
@@ -39,23 +38,6 @@ AppDataSource.initialize()
 
     // start express server
     app.listen(3000);
-
-    // insert new users for test
-    await AppDataSource.manager.save(
-      AppDataSource.manager.create(User, {
-        firstName: 'Timber',
-        lastName: 'Saw',
-        age: 27,
-      })
-    );
-
-    await AppDataSource.manager.save(
-      AppDataSource.manager.create(User, {
-        firstName: 'Phantom',
-        lastName: 'Assassin',
-        age: 24,
-      })
-    );
 
     console.log(
       'Express server has started on port 3000. Open http://localhost:3000/users to see results'

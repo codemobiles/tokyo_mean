c5894c5575f96593a1338ddbc863afa59a685267
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-04-10 11:19:59 +0700

    1234

--- workshops/tokioPOS/backend/src/controller/ProductController.ts
+++ workshops/tokioPOS/backend/src/controller/ProductController.ts
@@ -1,5 +1,10 @@
+import { AppDataSource } from '../data-source';
+import { Products } from '../entity/Products';
+
 export class ProductController {
+  private productRepository = AppDataSource.getMongoRepository(Products);
+
   query(request, response, next) {
-    return { result: 'product ok' };
+    return this.productRepository.find();
   }
 }
--- workshops/tokioPOS/backend/src/data-source.ts
+++ workshops/tokioPOS/backend/src/data-source.ts
@@ -1,13 +1,14 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import { Users } from './entity/Users';
+import { Products } from './entity/Products';
 
 export const AppDataSource = new DataSource({
   type: 'mongodb',
   database: 'tokioPOS',
   synchronize: true,
   logging: false,
-  entities: [Users],
+  entities: [Users, Products],
   migrations: [],
   subscribers: [],
 });

29a3f616908a720ecd270b224f975c8b7f093dbf
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-04-10 15:23:47 +0700

    1234

--- workshops/tokioPOS/src/app/components/header/header.component.ts
+++ workshops/tokioPOS/src/app/components/header/header.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { RestService } from 'src/app/services/rest.service';
+import { environment } from 'src/environments/environment';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  NavigationStart,
+  Router,
+} from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  @Output() sidenavToggle = new EventEmitter<void>();
+  pageName: string = '';
+  version = environment.version;
 
+  constructor(private router: Router, public rest: RestService) {}
+
+  ngOnInit() {}
+
+  onToggleSidenav() {
+    this.sidenavToggle.emit();
+  }
+
+  onClickSignout() {
+    localStorage.removeItem(environment.token);
+    this.router.navigate(['login']);
+  }
 }
--- workshops/tokioPOS/src/app/components/menu/menu.component.ts
+++ workshops/tokioPOS/src/app/components/menu/menu.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { RestService } from 'src/app/services/rest.service';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  @Output() closeSidenav = new EventEmitter<void>();
+  constructor(public rest: RestService) {}
 
+  ngOnInit() {}
+
+  onClose() {
+    this.closeSidenav.emit();
+  }
+
+  onLogout() {
+    this.onClose();
+  }
 }

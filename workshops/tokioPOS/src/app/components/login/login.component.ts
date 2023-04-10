2a69cad98d26042da7088dc628fc11aab5c9b12e
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-04-10 09:19:17 +0700

    1234

--- workshops/tokioPOS/src/app/components/login/login.component.ts
+++ workshops/tokioPOS/src/app/components/login/login.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { RestService } from 'src/app/services/rest.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  constructor(private rest: RestService) {}
 
+  hide = true;
+  async onSubmit(value: any) {
+    let result = await lastValueFrom(this.rest.login(value));
+    alert(JSON.stringify(result));
+  }
 }

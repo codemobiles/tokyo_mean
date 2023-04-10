e005e3c2534ce0a0e8a8e02e06ce6b76d5661099
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-04-10 14:13:48 +0700

    1234

--- workshops/tokioPOS/src/environments/environment.ts
+++ workshops/tokioPOS/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  node_static_url: 'http://localhost:3000',
+  node_static_url: 'http://localhost:8081',
   baseUrl: '',
   token: 'token',
   version: '1.1 BETA',

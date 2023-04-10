93c2903a060fc980625380d7a3b12b2e99ffb30e
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-04-10 14:26:15 +0700

    1234

--- workshops/tokioPOS/src/app/components/stock/stock.component.ts
+++ workshops/tokioPOS/src/app/components/stock/stock.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { Product } from 'src/app/models/product.model';
+import { RestService } from 'src/app/services/rest.service';
 
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
-  styleUrls: ['./stock.component.scss']
+  styleUrls: ['./stock.component.scss'],
 })
-export class StockComponent {
+export class StockComponent implements AfterViewInit {
+  dataArray: Product[] = [];
+
   constructor(public rest: RestService) {}
 
-
+  async ngAfterViewInit(): Promise<void> {
+    this.dataArray = await lastValueFrom(this.rest.getProducts());
+  }
 }

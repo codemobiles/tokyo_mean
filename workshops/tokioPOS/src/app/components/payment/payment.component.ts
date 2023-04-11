de0d4d0875a849575c9a351fa4d982b796b60e7a
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-04-11 15:10:15 +0700

    1234

--- workshops/tokioPOS/src/app/components/payment/payment.component.ts
+++ workshops/tokioPOS/src/app/components/payment/payment.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-payment',
@@ -8,8 +8,9 @@ import { Component, Input } from '@angular/core';
 export class PaymentComponent {
   @Input() productOrder: any;
   @Input() totalNumber: any;
+  @Output() onCompleted = new EventEmitter();
 
   handlePayment() {
-    
+    this.onCompleted.emit();
   }
 }

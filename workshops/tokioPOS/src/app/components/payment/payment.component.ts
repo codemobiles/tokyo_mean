f213af64fa67732fb68ed4ae07206b2591735125
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-04-11 15:17:48 +0700

    1234

--- workshops/tokioPOS/src/app/components/payment/payment.component.ts
+++ workshops/tokioPOS/src/app/components/payment/payment.component.ts
@@ -1,16 +1,89 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { SuccessDialogComponent } from './../success-dialog/success-dialog.component';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { RestService } from 'src/app/services/rest.service';
+import { TransactionRequest } from 'src/app/models/transaction.model';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
   styleUrls: ['./payment.component.scss'],
 })
-export class PaymentComponent {
+export class PaymentComponent implements OnInit {
   @Input() productOrder: any;
   @Input() totalNumber: any;
-  @Output() onCompleted = new EventEmitter();
+  @Output() onCompleted = new EventEmitter<void>();
+  givenNumber = '';
 
-  handlePayment() {
-    this.onCompleted.emit();
+  constructor(private rest: RestService, private dialog: MatDialog) {}
+
+  ngOnInit() {}
+
+  onClickGiven(addGiven: number) {
+    this.givenNumber = String(Number(this.givenNumber) + addGiven);
+  }
+
+  onClickReset() {
+    this.givenNumber = '0';
+  }
+
+  onClickExact() {
+    this.givenNumber = String(this.totalNumber);
+  }
+
+  convertToNumber(value: any): number {
+    return Number(value.replace(/,/g, ''));
+  }
+
+  public get changeMoney(): number {
+    let change = this.convertToNumber(this.givenNumber) - this.totalNumber;
+    if (change > 0) {
+      return change;
+    }
+    return 0;
+  }
+
+  public get isPaidEnough() {
+    if (Number(this.givenNumber) >= this.totalNumber) {
+      return true;
+    }
+    return false;
+  }
+
+  async onClickSubmit() {
+    let trans: TransactionRequest = {
+      total: this.totalNumber,
+      paid: Number(this.givenNumber),
+      change: Number(this.givenNumber) - this.totalNumber,
+      order_list: this.productOrder,
+
+      // Use default values
+      payment_type: 'cash',
+      payment_detail: 'full',
+      seller_id: 'sr0001',
+      buyer_id: 'by0000',
+      subtotal: 0,
+      discount: 0,
+      shipping_cost: 0,
+      comment: '',
+      tax_percent: 0,
+    };
+
+    try {
+      // Send transaction
+      await this.rest.sendTransaction(trans).toPromise();
+      const dialogConfirm = this.dialog.open(SuccessDialogComponent, {
+        data: {
+          title: 'การชำระ',
+          subtitle: 'สำเร็จ',
+        },
+      });
+
+      // Show success dialog
+      await dialogConfirm.afterClosed().toPromise();
+      this.onCompleted.emit();
+    } catch (e) {
+      alert(JSON.stringify(e));
+    }
   }
 }

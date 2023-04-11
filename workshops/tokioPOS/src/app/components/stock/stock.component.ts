b9f655554109652c4133f385bc6943b580b2fa9b
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-04-11 14:11:43 +0700

    1234

--- workshops/tokioPOS/src/app/components/stock/stock.component.ts
+++ workshops/tokioPOS/src/app/components/stock/stock.component.ts
@@ -1,39 +1,79 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { RestService } from './../../services/rest.service';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { lastValueFrom } from 'rxjs';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { Subject, lastValueFrom } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
 import { Product } from 'src/app/models/product.model';
-import { RestService } from 'src/app/services/rest.service';
-import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
   styleUrls: ['./stock.component.scss'],
 })
-export class StockComponent implements AfterViewInit {
-  dataSource: Product[] = [];
-  displayedColumns: string[] = [
+export class StockComponent implements OnInit, AfterViewInit {
+  displayedColumns = [
     'product_id',
     'image',
     'name',
-    'stock',
     'price',
+    'stock',
+    'action',
   ];
+  dataSource = new MatTableDataSource<Product>();
+  @ViewChild(MatPaginator, { static: false }) paginator!: MatPaginator;
+  @ViewChild(MatSort, { static: false }) sort!: MatSort;
+  searchValue = '';
+  searchTerm = new Subject<string>(); // subject is list of observable - receive new req by calling next function
 
-  constructor(public rest: RestService, private router: Router) {}
+  constructor(public rest: RestService, private dialog: MatDialog) {}
 
-  async ngAfterViewInit(): Promise<void> {
-    this.dataSource = await lastValueFrom(this.rest.getProducts());
-    // alert(JSON.stringify(this.dataArray));
+  async ngOnInit() {
+    this.dataSource.data = await lastValueFrom(await this.rest.getProducts());
   }
 
-  getImageUrl(image: string) {
-    return `${
-      environment.node_static_url
-    }/images/${image}?version=${Math.random()}`;
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
-  handleCreate() {
-    this.router.navigate(['stock/create']);
+  public get numberOfRow(): string {
+    if (this.dataSource.data) {
+      return this.dataSource.data.length.toString();
+    }
+    return '0';
+  }
+
+  async onClickDelete(row: any) {
+    const dialogConfirm = this.dialog.open(ConfirmDialogComponent, {
+      data: {
+        title: 'ลบสินค้า',
+        subtitle: 'คุณต้องการลบสินค้านี้ใช่หรือไม่?',
+        item: row,
+      },
+    });
+
+    let result = await lastValueFrom(dialogConfirm.afterClosed());
+    if (result) {
+      await lastValueFrom(this.rest.deleteProduct(row.product_id));
+      this.dataSource.data = await lastValueFrom(this.rest.getProducts());
+    }
+  }
+
+  doFilter(event: any) {
+    // do local
+    // this.dataSource.filter = filterValue.trim().toLowerCase()
+
+    // do remote
+    // this.dataSource.data = await this.rest.getProductByKeyword(filterValue).toPromise()
+    this.searchTerm.next(event.target.value);
+  }
+
+  clearSearch() {
+    this.searchValue = '';
+    this.searchTerm.next('');
   }
 }

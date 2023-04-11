9cbd7e746c7454a8e82a3dd1d1282f37a1e44445
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-04-11 11:20:08 +0700

    1234

--- workshops/tokioPOS/src/app/components/stock-edit/stock-edit.component.ts
+++ workshops/tokioPOS/src/app/components/stock-edit/stock-edit.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-stock-edit',
   templateUrl: './stock-edit.component.html',
-  styleUrls: ['./stock-edit.component.scss']
+  styleUrls: ['./stock-edit.component.scss'],
 })
-export class StockEditComponent {
+export class StockEditComponent implements OnInit, AfterViewInit {
+  constructor(private route: ActivatedRoute) {}
+  productId = '';
 
+  ngOnInit(): void {
+    this.productId = this.route.snapshot.params['id'];
+  }
+
+  ngAfterViewInit(): void {}
 }

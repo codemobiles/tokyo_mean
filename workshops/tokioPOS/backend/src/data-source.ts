d39f422fb8c48403575176f59bba523d47e80841
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-04-11 13:37:26 +0700

    1234

--- workshops/tokioPOS/backend/src/data-source.ts
+++ workshops/tokioPOS/backend/src/data-source.ts
@@ -2,13 +2,14 @@ import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import { Users } from './entity/Users';
 import { Products } from './entity/Products';
+import { Counters } from './entity/Counters';
 
 export const AppDataSource = new DataSource({
   type: 'mongodb',
   database: 'tokioPOS',
   synchronize: true,
   logging: false,
-  entities: [Users, Products],
+  entities: [Users, Products, Counters],
   migrations: [],
   subscribers: [],
 });

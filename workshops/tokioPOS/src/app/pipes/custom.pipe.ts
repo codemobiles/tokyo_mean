d6495c85f610baa14053563df9f7b6d6384d83af
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-04-11 15:19:16 +0700

    1234

---
+++ workshops/tokioPOS/src/app/pipes/custom.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'custom'
+})
+export class CustomPipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return value + "" + args[0];
+  }
+}
--- workshops/tokioPOS/src/app/pipes/qty.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { QtyPipe } from './qty.pipe';
-
-describe('QtyPipe', () => {
-  it('create an instance', () => {
-    const pipe = new QtyPipe();
-    expect(pipe).toBeTruthy();
-  });
-});

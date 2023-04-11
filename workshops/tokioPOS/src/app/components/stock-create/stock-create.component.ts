00e0816166cb85f0b1f5bd7ffe933cddeff21068
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-04-11 11:51:58 +0700

    1234

--- workshops/tokioPOS/src/app/components/stock-create/stock-create.component.ts
+++ workshops/tokioPOS/src/app/components/stock-create/stock-create.component.ts
@@ -1,19 +1,29 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-stock-create',
   templateUrl: './stock-create.component.html',
   styleUrls: ['./stock-create.component.scss'],
 })
-export class StockCreateComponent {
+export class StockCreateComponent implements OnInit {
   imageURL = null;
   imageFile = null;
   formProduct!: FormGroup;
+
+  ngOnInit(): void {
+    this.initForm();
+  }
 
   onSubmit() {}
 
   onChangeImage(event: any) {}
 
   onClickCancel() {}
+
+  initForm() {
+    this.formProduct = new FormGroup({
+      price: new FormControl(100, [Validators.required, Validators.min(10)]),
+    });
+  }
 }
